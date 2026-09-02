b592f8abb419826caa06b2ec254a299c942d9ac7
brasilia736211600-netizen <brasilia736211600@gmail.com>
2026-09-02 18:25:17 +0300

    feat: wire tracker cleaner into URL navigation

--- app/src/main/java/de/baumann/browser/unit/BrowserUnit.java
+++ app/src/main/java/de/baumann/browser/unit/BrowserUnit.java
@@ -93,16 +93,16 @@ public class BrowserUnit {
             return true;
         }
 
-        String regex = "^((ftp|http|https|intent)?://)"                      // support scheme
-                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" // ftp的user@
-                + "(([0-9]{1,3}\\.){3}[0-9]{1,3}"                            // IP形式的URL -> 199.194.52.184
-                + "|"                                                        // 允许IP和DOMAIN（域名）
-                + "(.)*"                                                     // 域名 -> www.
-                + "([0-9a-z_!~*'()-]+\\.)*"                                  // 域名 -> www.
-                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\."                    // 二级域名
-                + "[a-z]{2,6})"                                              // first level domain -> .com or .museum
-                + "(:[0-9]{1,4})?"                                           // 端口 -> :80
-                + "((/?)|"                                                   // a slash isn't required if there is no file name
+        String regex = "^((ftp|http|https|intent)?://)"
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?"
+                + "(([0-9]{1,3}\\.){3}[0-9]{1,3}"
+                + "|"
+                + "(.)*"
+                + "([0-9a-z_!~*'()-]+\\.)*"
+                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\."
+                + "[a-z]{2,6})"
+                + "(:[0-9]{1,4})?"
+                + "((/?)|"
                 + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(url).matches();
@@ -130,7 +130,7 @@ public class BrowserUnit {
                 query = URL_SCHEME_HTTP + query;
             }
 
-            return query;
+            return UrlTrackerCleaner.clean(query);
         }
 
         try {
