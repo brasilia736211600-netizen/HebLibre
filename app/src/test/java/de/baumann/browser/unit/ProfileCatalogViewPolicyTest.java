24dc0bab501ee270d038c83c1f81c3c0957f144a
brasilia736211600-netizen <brasilia736211600@gmail.com>
2026-09-06 22:05:29 +0300

    test: complete profile catalog view test syntax fix

--- app/src/test/java/de/baumann/browser/unit/ProfileCatalogViewPolicyTest.java
+++ app/src/test/java/de/baumann/browser/unit/ProfileCatalogViewPolicyTest.java
@@ -50,10 +50,10 @@ public class ProfileCatalogViewPolicyTest {
 
         assertEquals(Arrays.asList("m", "z", "a"),
                 ids(ProfileCatalogViewPolicy.filterAndSort(
-                        profiles, "", "", ProfileCatalogViewPolicy.SortMode.GROUP));
+                        profiles, "", "", ProfileCatalogViewPolicy.SortMode.GROUP)));
         assertEquals(Arrays.asList("a", "m", "z"),
                 ids(ProfileCatalogViewPolicy.filterAndSort(
-                        profiles, "", "", ProfileCatalogViewPolicy.SortMode.ID));
+                        profiles, "", "", ProfileCatalogViewPolicy.SortMode.ID)));
     }
 
     private List<String> ids(List<ProfileMetadata> profiles) {
