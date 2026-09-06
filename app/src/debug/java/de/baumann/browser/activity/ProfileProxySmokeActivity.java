37c6f3e8d10d50ec7a0d3a5479e2f8457ab0daa8
brasilia736211600-netizen <brasilia736211600@gmail.com>
2026-09-06 22:43:10 +0300

    test: allow profile proxy smoke harness to clear override

--- app/src/debug/java/de/baumann/browser/activity/ProfileProxySmokeActivity.java
+++ app/src/debug/java/de/baumann/browser/activity/ProfileProxySmokeActivity.java
@@ -8,13 +8,14 @@ import java.util.Collections;
 import de.baumann.browser.unit.ProfileCatalogStore;
 import de.baumann.browser.unit.ProfilePreferencesStore;
 
-/** Debug-only harness that configures the active profile to use the emulator host proxy. */
+/** Debug-only harness for setting/clearing the emulator's local CONNECT proxy. */
 public class ProfileProxySmokeActivity extends Activity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String profileId = ProfileCatalogStore.getActiveProfileId(this);
+        boolean clear = "clear".equals(getIntent().getStringExtra("mode"));
         ProfilePreferencesStore.restore(this, profileId, Collections.singletonMap(
-                "proxy_url", "s:http://10.0.2.2:18080"));
+                "proxy_url", clear ? "s:" : "s:http://10.0.2.2:18080"));
         ProfilePreferencesStore.restore(this, profileId, Collections.singletonMap(
                 "proxy_bypass", "s:"));
         finish();
